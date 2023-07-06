79fdd34889e79ab50693474479cb1e49fbac8d94
bergmani <bergman.ijp@gmail.com>
2023-07-05 21:00:50 -0600

    Se añaden funcionalidades a sign up y login

---
+++ js/validationLogin.js
@@ -0,0 +1,26 @@
+//let inputEmail = document.getElementById('floatingInputEmail');
+// let inputPassword = document.getElementById('floatingPassword');
+
+// const regexEmail = /^[\w.+\-]+@gmail\.com$/;
+//const regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+
+// inputEmail.addEventListener('input', input => {
+//         if(!regexEmail.test(input.target.value)){
+//             input.preventDefault();
+//             input.stopPropagation();
+//             console.log(inputEmail)
+//         } else {
+//             inputEmail.classList.add('was-validated');
+//         }
+// })
+
+// inputPassword.addEventListener('input', input => {
+//         if(regexPassword.test(input.target.value)){
+//             input.preventDefault()
+//             input.stopPropagation()
+//         }
+//         inputPassword.classList.add('was-validated')
+// })
+
+    
